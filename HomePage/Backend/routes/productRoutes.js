3df3b0201d8dd1a4a0251e1541b121bcb4da9284
vagmin27 <vagmin2727@gmail.com>
2025-07-09 17:56:35 +0530

    Delete HomePage/Backend directory

--- HomePage/Backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-// homepage/backend/routes/productRoutes.js
-import express from 'express';
-import { getProducts } from '../controllers/productController.js';
-
-const router = express.Router();
-
-router.get('/', getProducts);
-
-export default router;
